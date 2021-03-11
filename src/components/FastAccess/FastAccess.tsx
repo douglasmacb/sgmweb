c17d0bcc81067ce8c861ee5af0f9db5a37c133cc
douglasmacb <douglasmacb@outlook.com>
2021-03-11 08:44:46 -0300

    feat: go back feature implemented at TaxPage

--- src/components/FastAccess/FastAccess.tsx
+++ src/components/FastAccess/FastAccess.tsx
@@ -3,6 +3,7 @@ import { Search } from '../Search/Search'
 import history from '../../history'
 
 const SearchByCpfCnpj = (event: any) => {
+    console.log(event)
     history.push('/tax')
 }
 
@@ -10,10 +11,11 @@ const searchProcess = (event: any) => {
 }
 
 export const FastAccess: React.FC = () => {
+
     return (
         <div className="fast-access">
             <div className="search-bar">
-                <Search title="Consultar IPTU/ITR por CPF / CNPJ" formValues={SearchByCpfCnpj} maxLength={14} />
+                <Search title="Consultar Imposto Territorial por CPF / CNPJ" formValues={SearchByCpfCnpj} maxLength={14} />
                 <Search title="Consultar Processos" mask="999999" formValues={searchProcess} />
             </div>
         </div>
--- src/pages/Tax/TaxPage.tsx
+++ src/pages/Tax/TaxPage.tsx
@@ -1,33 +1,45 @@
 import React from 'react'
+import { History, LocationState } from 'history'
+import { Button } from '../../components/Button'
 import './TaxPage.css'
 
-class TaxPage extends React.Component {
+interface Props {
+    history: History<LocationState>
+}
+
+class TaxPage extends React.Component<Props> {
+
+    goBack = (): void => {
+        this.props.history.goBack()
+    }
+
     render() {
         return (
             <>
-                <div>
-                    <h2 className="page-title">Consulta IPTU / ITR</h2>
-                    <table>
+                <h2 className="page-title">Consulta de Imposto Territorial</h2>
+                <div className="tax-container">
+                    <table className="tax-table">
                         <tbody>
                             <tr>
-                                <td>John Lennon</td>
-                                <td>Rhythm Guitar</td>
+                                <td className="tax-table-name">John Lennon</td>
+                                <td className="tax-table-value">Rhythm Guitar</td>
                             </tr>
                             <tr>
-                                <td>Paul McCartney</td>
-                                <td>Bass</td>
+                                <td className="tax-table-name">Paul McCartney</td>
+                                <td className="tax-table-value">Bass</td>
                             </tr>
                             <tr>
-                                <td>George Harrison</td>
-                                <td>Lead Guitar</td>
+                                <td className="tax-table-name">George Harrison</td>
+                                <td className="tax-table-value">Lead Guitar</td>
                             </tr>
                             <tr>
-                                <td>Ringo Starr</td>
-                                <td>Drums</td>
+                                <td className="tax-table-name">Ringo Starr</td>
+                                <td className="tax-table-value">Drums</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+                <Button cName="tax-btn" onClick={this.goBack}>Voltar</Button>
             </>
         )
     }
